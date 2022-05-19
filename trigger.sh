61290c9bb23e263454ac5e92ba47a436984bb2ba
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-05-19 19:13:54 +0530

    update trigger

--- trigger.sh
+++ trigger.sh
@@ -2,7 +2,7 @@
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_dd` ]]
 then 
-	docker run -p 8000:8000 -d -v $HOME/ip_vol:/ip_vol --name closet_dd m1v1n/closet_dojo
+	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol -v --name closet_dd m1v1n/closet_dojo
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_octant` ]]
@@ -10,4 +10,4 @@ then
 	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant m1v1n/octant
 fi
 
-docker run -v /var/run.docker.sock:/var/run/docker.sock -v /config:/root/config -e REPO='' -d m1v1n/closet_start
+docker run -v /var/run.docker.sock:/var/run/docker.sock -v $HOME/config:/root/config -e REPO='' -d m1v1n/closet_start
